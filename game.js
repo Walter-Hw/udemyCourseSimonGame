84228f4bf34c6e62d9145d013024a6b376c859af
Walter Huang <autumnhao@gmail.com>
2021-03-27 17:00:04 -0700

    Add some functions that can reflect the realtime score of game. And refactor code to adapt to the ES6.

--- game.js
+++ game.js
@@ -1,29 +1,56 @@
+let buttonColors = ['red', 'blue', 'green', 'yellow'];
 let userClickedPattern = [];
 let gamePattern = [];
-let buttonColors = ['red', 'blue', 'green', 'yellow'];
+let level = 0;
+let started = false;
+
+$(document).on('keypress', () => {
+
+  if (!started) {
+    nextSequence();
+    $('#level-title').text('Level ' + level);
+    started = true;
+  }
+})
+
+$('.btn').on('click', (e) => {
+
+  let userChosenColor = $(e.currentTarget).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+})
 
 let nextSequence = () => {
+
+  level++;
+  $('#level-title').text('Level ' + level);
+
   let randomNumber = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+
   $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColor);
+
 };
 
-$('btn').on('click', () => {
-  let uesrChosenColor = $(this).attr('id');
-  userClickedPattern.push(uesrChosenColor);
-  playSound(uesrChosenColor);
-})
+// Helper Function
 
 let playSound = (name) => {
+
   let audio = new Audio(`sounds/${name}.mp3`);
   audio.play();
-}
+
+};
 
 let animatePress = (currentColor) => {
+
   $(`#${currentColor}`).addClass('pressed');
-  setTimeOut(() => {
+  setTimeout(() => {
     $(`#${currentColor}`).removeClass('pressed');
   }, 100);
-}
+
+};
